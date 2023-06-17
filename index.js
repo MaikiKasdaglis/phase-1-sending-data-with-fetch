6b3828c3b46f371cd41b5a3a0ca5dda42b4d200e
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-06-16 19:16:28 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+// function submitData(userName, userEmail) {
+//     fetch(http://localhost:3000/users, configurationObject);
+// }
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       //username/email go here? 
+//     }),
+//   };
+
+function submitData(name, email) {
+   return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    }, 
+    body: JSON.stringify ({name, email})
+  })
+  .then(r => r.json())
+  .then(obj => document.body.innerHTML = obj.id)
+  .catch(e => document.body.innerHTML = e.message)
+
+}
